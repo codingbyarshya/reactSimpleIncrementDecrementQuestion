45982a7a40f3f6e20c61afaf08c23b66d7eff500
codingbyarshya <117349670+codingbyarshya@users.noreply.github.com>
2023-06-26 13:18:31 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Blog from './Counter.jsx';
+
+
+const App = () => {
+  return ( <div>
+           <Blog/>
+      </div>
+  );
+};
+
+export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
